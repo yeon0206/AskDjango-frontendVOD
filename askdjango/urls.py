7f7537e5f87288768ae6fed44ba1e3682f54da0e
GIYEON KWON <brucek0206@gmail.com>
2017-11-18 18:16:33 +0900

    JSON PostList뷰를 직접 구현

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'', include('blog.urls', namespace='blog')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404,render, resolve_url
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView,UpdateView,DeleteView
@@ -58,3 +59,12 @@ class CommentDeleteView(DeleteView):
         return resolve_url(self.object.post)
 
 comment_delete = CommentDeleteView.as_view()
+
+def post_list_json(request):
+    qs = Post.objects.all()
+
+    post_list = []
+    for post in qs:
+        post_list.append({'id': post.id,'title': post.title,'content' : post.content})
+    
+    return JsonResponse(post_list, safe=False)
