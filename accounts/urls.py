f693574018bc8cbeae0e00c18753910e10a68bdc
GIYEON KWON <brucek0206@gmail.com>
2017-11-19 18:46:03 +0900

    login구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 from . import views
 urlpatterns = [
     url(r'^signup/$', views.SignupFormView.as_view(), name='signup'),
+    url(r'^login/$', login, name='login', kwargs={'template_name': 'accounts/login.html'}),
 ]
