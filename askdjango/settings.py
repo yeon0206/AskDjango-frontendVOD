6c9db894408586a8ecafcea13263ed0b1b26c976
GIYEON KWON <brucek0206@gmail.com>
2017-11-19 17:31:15 +0900

    댓글 이미지에 대해서 썸네일 처리

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'bootstrap3',
+    'easy_thumbnails',
+    'rest_framework',
     'blog',
 ]
 
