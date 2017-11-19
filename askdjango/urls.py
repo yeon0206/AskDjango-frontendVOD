6b99cf651c24605ade165f9cd85dbda6733c7a0c
GIYEON KWON <brucek0206@gmail.com>
2017-11-19 18:24:21 +0900

    added accounts app

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'', include('blog.urls', namespace='blog')),
 ]
 
