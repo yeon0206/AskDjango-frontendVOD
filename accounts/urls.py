1d942f58fa4bd883bdb25437b35a04d0d3229aad
GIYEON KWON <brucek0206@gmail.com>
2017-11-19 20:14:09 +0900

    ajax/model을 통한 로그인 / 회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 from django.conf.urls import url
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import logout
 from . import views
 urlpatterns = [
     url(r'^signup/$', views.SignupFormView.as_view(), name='signup'),
-    url(r'^login/$', login, name='login', kwargs={'template_name': 'accounts/login.html'}),
+    url(r'^login/$', views.LoginView.as_view(), name='login', kwargs={'template_name': 'accounts/login.html'}),
     url(r'^logout/$', logout, name='logout', kwargs={'next_page': settings.LOGIN_URL }),
     url(r'^profile/$', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,38 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView as AuthLoginView
 from django.contrib.auth.forms import UserCreationForm
+from django.http import JsonResponse
+from django.shortcuts import render
 from django.views.generic import CreateView
 
 class SignupFormView(CreateView):
     form_class = UserCreationForm
     template_name = 'accounts/signup_form.html'
     success_url = settings.LOGIN_URL
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if self.request.is_ajax():
+            return JsonResponse({'next_url': self.get_success_url()})
+        return response
+
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return ['accounts/_signup_form.html']
+        return ['accounts/signup_form.html']
+
+class LoginView(AuthLoginView):
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if self.request.is_ajax():
+            return JsonResponse({'next_url': self.get_success_url()})
+        return response
+
+    def get_template_names(self):
+        if self.request.is_ajax():
+            return ['accounts/_login.html']
+        return ['accounts/login.html']
 
 @login_required
 def profile(request):
