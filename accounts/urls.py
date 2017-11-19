84204a6d65635800ece4ae02d21b084c163ea379
GIYEON KWON <brucek0206@gmail.com>
2017-11-19 19:02:01 +0900

    유저 프로필 페이지 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^signup/$', views.SignupFormView.as_view(), name='signup'),
     url(r'^login/$', login, name='login', kwargs={'template_name': 'accounts/login.html'}),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
@@ -7,3 +8,7 @@ class SignupFormView(CreateView):
     form_class = UserCreationForm
     template_name = 'accounts/signup_form.html'
     success_url = settings.LOGIN_URL
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
