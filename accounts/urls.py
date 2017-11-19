eb3ef2ddd642bde28f9d92cd74ca97c81a0f89d6
GIYEON KWON <brucek0206@gmail.com>
2017-11-19 18:41:41 +0900

    회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-
+from . import views
 urlpatterns = [
-    
+    url(r'^signup/$', views.SignupFormView.as_view(), name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 
-# Create your views here.
+class SignupFormView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/signup_form.html'
+    success_url = settings.LOGIN_URL
